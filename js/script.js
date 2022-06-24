2ae1182329a3a64da440fe087baf969a0c8a837d
SamueleLettieri <samuele2073@gmail.com>
2022-06-24 17:29:59 +0200

    campo minato, da completare

--- js/script.js
+++ js/script.js
@@ -6,6 +6,26 @@ function newElementSquare (){
     return newElement;
 };
 
+function numberGenerator(blacklist, numberMin, numberMax){
+    let numberRandom;
+ 
+    let validNumber = false;
+ 
+    while (validNumber === false){
+        numberRandom = Math.round(Math.random() * (numberMax - numberMin) + numberMin);
+ 
+        if( !blacklist.includes(numberRandom)){
+         validNumber = true;
+        }
+    }
+ 
+    return numberRandom;
+}
+
+let blacklistNumber = [];
+console.log(blacklistNumber)
+
+
 
 
 const play = document.getElementById("button");
@@ -25,28 +45,34 @@ play.addEventListener('click', function(){
     }
     
     console.log(livello) 
-    for (let i = 1; i <= dimensione; i++){
+    for (let i = 17; i <= dimensione; i++){
         let newSquare = newElementSquare ();
         if(dimensione === 81){
             newSquare.classList.add("square_difficile");
         }else if(dimensione === 49) {
             newSquare.classList.add("square_ultra_difficile");
         }
-
-
+        
+        newSquare.innerHTML = i
+        
         newSquare.addEventListener('click', function(){
             newSquare.classList.add("active")
             console.log(`hai cliccato ${i}`)
         });
-    
-        newSquare.innerHTML = i
-    
+        
         container.append(newSquare);
-    };
+        
+    }
+    blacklistNumber = [];
+    for ( let i = 0; i < 16; i++){
+
+        let boom = numberGenerator(blacklistNumber, 1, dimensione);
+
+        blacklistNumber.push(boom); 
+    }
+    console.assert(blacklistNumber);
 })
 
 
 
-function numberGenerator (blacklist, numberMin, numberMax){
-   
-}
+
