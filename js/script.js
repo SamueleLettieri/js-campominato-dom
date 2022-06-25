b2edac281a97d0a97d7f03f2f0e3d0b7b5117857
SamueleLettieri <samuele2073@gmail.com>
2022-06-25 17:01:16 +0200

    Campo Minato

--- js/script.js
+++ js/script.js
@@ -33,7 +33,7 @@ const select = document.getElementById("difficolta");
 
 play.addEventListener('click', function(){
     container.innerHTML = "";
-    
+
     let livello = select.value;
     let dimensione; 
     if(livello === "Normale"){
@@ -43,36 +43,60 @@ play.addEventListener('click', function(){
     } else if(livello === "Ultra-difficile"){
         dimensione = 49;
     }
-    
     console.log(livello) 
-    for (let i = 17; i <= dimensione; i++){
-        let newSquare = newElementSquare ();
-        if(dimensione === 81){
-            newSquare.classList.add("square_difficile");
-        }else if(dimensione === 49) {
-            newSquare.classList.add("square_ultra_difficile");
+    blacklistNumber = [];
+    
+
+    for (let i = 1; i <= dimensione; i++){
+        let number = numberGenerator(blacklistNumber, 1, 100);
+        blacklistNumber.push(number); 
+        if (number <= 16){
+            let newBoomSquare = newElementSquare ();
+            if(dimensione === 81){
+                newBoomSquare.classList.add("square_difficile");
+            }else if(dimensione === 49) {
+                newBoomSquare.classList.add("square_ultra_difficile");
+            }
+            blacklistNumber.push(number);
+            newBoomSquare.addEventListener('click', function(){
+                newBoomSquare.classList.add("boom")
+                console.log(`hai cliccato una bomba`)
+
+                if(newBoomSquare === newBoomSquare){
+                    container.innerHTML = "Hai perso. Per rigiocare premi play"
+                    console.log(container)
+                }
+            });
+            newBoomSquare.innerHTML = i;
+            container.append(newBoomSquare);
+        
+        }else if(number > 16){
+            let newSquare = newElementSquare ();
+            if(dimensione === 81){
+                newSquare.classList.add("square_difficile");
+            }else if(dimensione === 49) {
+                newSquare.classList.add("square_ultra_difficile");
+            }
+            
+            blacklistNumber.push(number);
+            newSquare.addEventListener('click', function(){
+                newSquare.classList.add("active")
+                console.log(`hai cliccato ${i}`)
+            });
+            newSquare.innerHTML = i
+            container.append(newSquare);
+    
         }
         
-        newSquare.innerHTML = i
+    
         
-        newSquare.addEventListener('click', function(){
-            newSquare.classList.add("active")
-            console.log(`hai cliccato ${i}`)
-        });
-        
-        container.append(newSquare);
-        
-    }
-    blacklistNumber = [];
-    for ( let i = 0; i < 16; i++){
 
-        let boom = numberGenerator(blacklistNumber, 1, dimensione);
-
-        blacklistNumber.push(boom); 
     }
-    console.assert(blacklistNumber);
+ 
 })
 
 
+
+        
 
 
